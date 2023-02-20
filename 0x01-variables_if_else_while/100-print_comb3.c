c18059e0b19552ec042dc4bf0e3e915a6806aea4
Aniekeme <aniekemeusanga@gmail.com>
2023-02-20 02:45:23 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - A program to print all possible outcome of the combinations of two digits
+ * main - print all possible outcome of the combinations of two digits
  *
  * Return: 0
 */
