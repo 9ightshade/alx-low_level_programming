44d679b487f312145975fc4e1908b9eaf726718e
Aniekeme <aniekemeusanga@gmail.com>
2023-02-23 17:08:40 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/*
+ * main - causes an infinite loop
+ * Returned ;0
+ */
+
+int main (void)
+{
+	int i;
+
+	pintf("infinte loop incoming :(\n");
+
+	i = o;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*
+	 putchar(i);*/
+       /*}*/
+
+	printf("infinite loop avoided \\");
+
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
